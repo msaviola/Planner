70c2bb0d78f852eb35c4f0253b7e61044e113142
Mariella Saviola <msaviola@sandiego.edu>
2020-08-01 16:29:21 -0700

    add tasks functions

--- script.js
+++ script.js
@@ -22,13 +22,50 @@
 
 // functions
 function displayDate() {
-    const currentDate = moment().format('H')
-    const currentDateEl = $("#currentDay");
-    currentDateEl.text(currentDate)
+    const currentDate = moment().format('dddd, MMMM DD, YYYY');
+    console.log(currentDate);
+    $("#currentDay").text(JSON.stringify(currentDate))
+    //currentDate.text(currentDate)
+  }
+
+  
+
+  // $("#savBtn").on("click", function (event) {
+  //   // event.preventDefault() prevents submit button from trying to send a form.
+  //   // Using a submit button instead of a regular button allows the user to hit
+  //   // "Enter" instead of clicking the button if desired
+  //   event.preventDefault();
+  //   console.log("pressed save btn");
+
+  //   const taskToDo=$('#task').val();
+  //   window.localStorage;
+  //   //localStorage.setItem("key", "value");
+  //   //movies.push(movieName);
+  // })
+  $("#add-task").on("click", function(event) {
+    // event.preventDefault() prevents submit button from trying to send a form.
+    // Using a submit button instead of a regular button allows the user to hit
+    // "Enter" instead of clicking the button if desired
+    event.preventDefault();
+    console.log("pressed save btn");
+    //var movie = $("#movie-input").val();
+
+    var taskName=$('#task-input').val();
+    console.log(taskName);
+    localStorage.setItem("key", taskName);
+    console.log(window.localStorage);
+    
+  })
+
+
+  function getSavedTasks(){
+    var saved = localStorage.getItem("key");
   }
   
   
   
   $(document).ready(function() {
     displayDate()
+    console.log(window.localStorage);
+    
   })

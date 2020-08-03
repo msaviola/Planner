f89e194e471fde69b88a6c59dd310e59a703ad9d
Mariella Saviola <msaviola@sandiego.edu>
2020-08-03 11:35:52 -0700

    working local storage

--- script.js
+++ script.js
@@ -20,50 +20,97 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-  //var saved = localStorage.getItem("key");
+    
+
 
 // functions
 function displayDate() {
     const currentDate = moment().format('dddd, MMMM DD, YYYY');
     console.log(currentDate);
     $("#currentDay").text(JSON.stringify(currentDate))
-    //currentDate.text(currentDate)
+   
   }
 
-  
 
-  // $("#savBtn").on("click", function (event) {
-  //   // event.preventDefault() prevents submit button from trying to send a form.
-  //   // Using a submit button instead of a regular button allows the user to hit
-  //   // "Enter" instead of clicking the button if desired
-  //   event.preventDefault();
-  //   console.log("pressed save btn");
 
-  //   const taskToDo=$('#task').val();
-  //   window.localStorage;
-  //   //localStorage.setItem("key", "value");
-  //   //movies.push(movieName);
-  // })
-  $("#add-task").on("click", function(event) {
+
+  $(".saveBtn").on("click", function(event) {
     // event.preventDefault() prevents submit button from trying to send a form.
-    // Using a submit button instead of a regular button allows the user to hit
     // "Enter" instead of clicking the button if desired
+
     event.preventDefault();
     console.log("pressed save btn");
-    //var movie = $("#movie-input").val();
+    
 
-    var taskName=$('#task-input').val();
-    console.log(taskName);
-    localStorage.setItem("key", taskName);
-    console.log(window.localStorage);
+    var taskName0=$('#task-input0').val();    
+    localStorage.setItem("task0", taskName0);
+
+    var taskName1=$('#task-input1').val();    
+    localStorage.setItem("task1", taskName1);
+
+    var taskName2=$('#task-input2').val();    
+    localStorage.setItem("task2", taskName2);
+
+    var taskName3=$('#task-input3').val();    
+    localStorage.setItem("task3", taskName3);
+
+    var taskName4=$('#task-input4').val();    
+    localStorage.setItem("task4", taskName4);
+
+    var taskName5=$('#task-input5').val();    
+    localStorage.setItem("task5", taskName5);
+
+    var taskName6=$('#task-input6').val();    
+    localStorage.setItem("task6", taskName6);
+
+    var taskName7=$('#task-input7').val();    
+    localStorage.setItem("task7", taskName7);
+
+    var taskName8=$('#task-input8').val();    
+    localStorage.setItem("task8", taskName8);
+
+    var taskName9=$('#task-input9').val();    
+    localStorage.setItem("task9", taskName9);
+
+
+    
     
   })
 
 
   function getSavedTasks(){
-    var saved = localStorage.getItem("key");
-    $('#task-input').text(saved);
+
     console.log("getting previous tasks");
+
+    var saved0 = localStorage.getItem("task0");
+    $('#task-input0').text(saved0);
+
+    var saved1 = localStorage.getItem("task1");
+    $('#task-input1').text(saved1);
+
+    var saved2 = localStorage.getItem("task2");
+    $('#task-input2').text(saved2);
+
+    var saved3 = localStorage.getItem("task3");
+    $('#task-input3').text(saved3);
+
+    var saved4 = localStorage.getItem("task4");
+    $('#task-input4').text(saved4);
+
+    var saved5 = localStorage.getItem("task5");
+    $('#task-input5').text(saved5);
+
+    var saved6 = localStorage.getItem("task6");
+    $('#task-input6').text(saved6);
+
+    var saved7 = localStorage.getItem("task7");
+    $('#task-input7').text(saved7);
+
+    var saved8 = localStorage.getItem("task8");
+    $('#task-input8').text(saved8);
+
+    var saved9 = localStorage.getItem("task9");
+    $('#task-input9').text(saved9);
 
   }
   
